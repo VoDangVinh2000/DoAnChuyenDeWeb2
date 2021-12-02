29e195a9ff72886890399831e1f5a558680a2cdf
VoDangVinh2000 <01686228222vinh@gmail.com>
2021-12-02 22:02:46 +0700

    push again

---
+++ Laravel-VueJs/app/Http/Controllers/Api/SectionArticlesPostsController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\SectionArticlesPosts;
+use Illuminate\Http\Request;
+
+class SectionArticlesPostsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //get section article post with category_article_post_id
+        $SectionArticlePost_ID = SectionArticlesPosts::join('category_article_post','section_articles_posts.category_article_post_id','=','category_article_post.id')
+        ->select('section_articles_posts.*')->where(['section_articles_posts.category_article_post_id' => $id])->get();
+        return response($SectionArticlePost_ID,200);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ Laravel-VueJs/app/Models/CategoryArticlePost.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CategoryArticlePost extends Model
+{
+    protected $table = 'category_article_post';
+    use HasFactory;
+}
